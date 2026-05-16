bafae4f57bce386303a5965070876ba0c4b36a5a
samuel940 <70614085+samuel940@users.noreply.github.com>
2026-05-16 01:41:10 -0400

    Refactor dataRetrieving.js by removing unused functions
    Removed unused functions for retrieving professor and course names. Updated URL construction for fetching reviews.

--- dataRetrieving.js
+++ dataRetrieving.js
@@ -3,66 +3,16 @@
 const alasql = require("alasql");
 const fs = require("fs");
 
-
-async function professorNames() {
-  const TerpProfessorURL = `https://planetterp.com/api/v1/professors?`;
-  try {
-    const response = await fetch(TerpProfessorURL);
-    console.log(response);
-    
-    const json = await response.json();
-    
-
-    return json.map(prof => prof.name);;
-
-  } catch (error) {
-			// Code to handle the error
-			console.error("Invalid JSON input:", error.message);
-      return [];
-	}
-
-  
-  
-
-}
-
-async function courseNames() {
-  const TerpCoursesURL = `https://planetterp.com/api/v1/courses?`;
-  try {
-    const response = await fetch(TerpCoursesURL);
-    console.log(response);
-    
-    const json = await response.json();
-    json.forEach(course => {
-      courses.push(course.name);
-    });
-
-    return json.map(course => course.name);;
-
-  } catch (error) {
-			// Code to handle the error
-			console.error("Invalid JSON input:", error.message);
-      return [];
-	}
-  
-
-}
-
 async function rateAllClassesForProfessor(name) {
 
   const encodedName = encodeURIComponent(name);
 
-  const url =
-    `https://planetterp.com/api/v1/professor?name=${encodedName}&reviews=true`;
+  const url = `https://planetterp.com/api/v1/professor?name=${encodedName}&reviews=true`;
   
-  console.log("rateAllClassesForProfessor called with name:", name);
-  console.log(url);
   try {
 
     const response = await fetch(url);
-
     const json = await response.json();
-
     const allReviews = json.reviews;
 
     // SQL query
@@ -78,31 +28,21 @@ async function rateAllClassesForProfessor(name) {
 
     results = results.filter(result => result.course !== null);
 
-    //console.log(allReviews);
-    //console.log(results);
     return results;
 
   } catch(error) {
-
     console.error("Invalid JSON input:", error.message);
-
     return [];
   }
 }
 
 async function rateAllProfessorsForClass(name) {
-
-  
-
-  const url =
-    `https://planetterp.com/api/v1/course?name=${name}&reviews=true`;
+  const url = `https://planetterp.com/api/v1/course?name=${name}&reviews=true`;
 
   try {
 
     const response = await fetch(url);
-
     const json = await response.json();
-
     const allReviews = json.reviews;
 
     // SQL query
@@ -121,9 +61,7 @@ async function rateAllProfessorsForClass(name) {
     return results;
 
   } catch(error) {
-
     console.error("Invalid JSON input:", error.message);
-
     return [];
   }
 }
